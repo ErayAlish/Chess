286c90f31c562000e0b5a52dc371b7dcb4fb4347
Bin Aktiv Du Passiv ☯︎︎馬 <erayalish8848@gmail.com>
2024-01-29 20:47:10 +0100

    a1

---
+++ main.js
@@ -0,0 +1,24 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerdDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop ,knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn ,pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn ,pawn, pawn,
+    rook, knight, bishop, queen, king, bishop ,knight, rook,
+]
+
+function createBoard(){
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        squar.classList.add('beige')
+        gameBoard.append(square)
+    })
+}
